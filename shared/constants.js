f28eb553e464a0edc4d721387274a9754922b190
anyl92 <yulim9592@gmail.com>
2025-03-23 23:08:19 +0900

    feat: 봇 호출 시간 변경으로 메시지 수정

--- shared/constants.js
+++ shared/constants.js
@@ -1,7 +1,7 @@
 export const CHANNER_NAME = "4_피크민또";
 export const WALK_TEXT =
-  "리마인더: :pikmin_happy:월요일부터 시작되는 100,000보 걷기 챌린지 참여하실 분께선 스레드 댓글로 남겨주세요. 랜덤 매칭한 결과는 월요일 아침에 발표됩니다.:pikmin_run:";
+  "리마인더: :pikmin_happy:월요일부터 시작되는 100,000보 걷기 챌린지 참여하실 분께선 스레드 댓글로 남겨주세요. 포루라기가 랜덤 매칭한 결과는 일요일 22시에 발표됩니다.:pikmin_run:";
 export const FLOWER_TEXT =
-  "리마인더: :pikmin_gogo:월요일부터 시작되는 30,000송이 심기 챌린지 참여하실 분께선 스레드 댓글로 남겨주세요. 랜덤 매칭한 결과는 월요일 아침에 발표됩니다.:bouquet:";
+  "리마인더: :pikmin_gogo:월요일부터 시작되는 30,000송이 심기 챌린지 참여하실 분께선 스레드 댓글로 남겨주세요. 포루라기가 랜덤 매칭한 결과는 일요일 22시에 발표됩니다.:bouquet:";
 
 export const MAX_PEOPLE = 5;
